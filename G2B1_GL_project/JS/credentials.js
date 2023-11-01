ede81601073b55d285906e8912133ab051e7affa
jaymin1088 <139453693+jaymin1088@users.noreply.github.com>
2023-11-01 12:23:34 +0530

    file uploaded
    file uploaded

--- G2B1_GL_project/JS/credentials.js
+++ G2B1_GL_project/JS/credentials.js
@@ -1,4 +1,6 @@
-const credentials = [{
-    username : 'admin',
-    password : '1234'
-}]
+const credentials = [
+  {
+    username: "admin",
+    password: "1234",
+  },
+];
--- G2B1_GL_project/JS/login.js
+++ G2B1_GL_project/JS/login.js
@@ -1,21 +1,19 @@
-let userName = document.getElementById('username');
-let pass = document.getElementById('password');
-let form = document.getElementById('form');
-let error = document.getElementById('error');
+let userName = document.getElementById("username");
+let pass = document.getElementById("password");
+let form = document.getElementById("form");
+let error = document.getElementById("error");
 
+form.addEventListener("submit", submitHandler);
 
-form.addEventListener('submit', submitHandler);
+function submitHandler(event) {
+  event.preventDefault();
 
-function submitHandler(event){
-event.preventDefault();
-
-for(let val of credentials){
+  for (let val of credentials) {
     console.log(val.username, userName.value);
-    if(val.username===userName.value&&val.password===pass.value){
-        location.replace('../HTML/index.html')
-    }else{
-        error.style.display = 'block';
+    if (val.username === userName.value && val.password === pass.value) {
+      location.replace("../HTML/index.html");
+    } else {
+      error.style.display = "block";
     }
-}
-
+  }
 }
